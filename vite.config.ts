c38dd5e1f8930db429b4ba26ec68f4d2f5e0a13b
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2024-01-23 12:16:42 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/scum-bunker-hack/',
+	base: '/scum-bunker-door-hack/',
 	plugins: [react()],
 });
